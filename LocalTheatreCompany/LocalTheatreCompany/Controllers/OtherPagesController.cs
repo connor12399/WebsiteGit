9105ce4fcbcde88485bee1e4ea97d6ffb37e2c23
Connor <mcgrath211@hotmail.com>
2022-05-23 12:01:56 +0100

    Added Face

---
+++ LocalTheatreCompany/LocalTheatreCompany/Controllers/OtherPagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocalTheatreCompany.Controllers
+{
+    public class OtherPagesController : Controller
+    {
+        // GET: OtherPages
+        public ActionResult LandingPage()
+        {
+            return View();
+        }
+    }
+}
